ffd3f6d1025acb1b4f807fa66ba27bb9eab109c6
valentin <valentinvazquez1105@gmail.com>
2025-11-18 17:23:29 +0000

    Punto_B.sh a RTA_Examen_20251117

--- Punto_B.sh
+++
@@ -1,6 +0,0 @@
-
-sudo nano /usr/local/bin/vazquezAltaUser-Groups.sh
-
-sudo chmod +x /usr/local/bin/vazquezAltaUser-Groups.sh
-
-sudo /usr/local/bin/vazquezAltaUser-Groups.sh vagrant ~/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
