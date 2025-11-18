08f604b50dd0fcae982f20a8ccabac2c8a74268e
valentin <valentinvazquez1105@gmail.com>
2025-11-18 03:18:44 +0000

    Moví Punto_B.sh a RTA_Examen_20251117

---
+++ RTA_Examen_20251117/Punto_B.sh
@@ -0,0 +1,6 @@
+
+sudo nano /usr/local/bin/vazquezAltaUser-Groups.sh
+
+sudo chmod +x /usr/local/bin/vazquezAltaUser-Groups.sh
+
+sudo /usr/local/bin/vazquezAltaUser-Groups.sh vagrant ~/UTN-FRA_SO_Examenes/202406/bash_script/Lista_Usuarios.txt
